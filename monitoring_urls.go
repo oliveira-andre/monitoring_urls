66a321f325af165707a11d2cadadf59c70f785f5
Andre Oliveira <desmonplays@gmail.com>
2020-08-01 15:22:40 -0400

    feat: getting keyboard input

--- monitoring_urls.go
+++ monitoring_urls.go
@@ -8,4 +8,11 @@ func main() {
 
   fmt.Println("Hello mr.", name)
   fmt.Println("this project is on version", version)
+
+  fmt.Println("1 - initialize monitoring")
+  fmt.Println("2 - show logs")
+  fmt.Println("0 - exit")
+
+  var choosed_option int
+  fmt.Scan(&choosed_option)
 }
