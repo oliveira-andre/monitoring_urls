2ff0112d91388da6a508b037643ed90eae7fe014
Andre Oliveira <desmonplays@gmail.com>
2020-08-01 16:14:22 -0400

    feat: adding functions welcome, show options and choose options

--- monitoring_urls.go
+++ monitoring_urls.go
@@ -3,16 +3,39 @@ package main
 import "fmt"
 
 func main() {
+  welcome()
+  showOptions()
+  choosedOption := chooseOption()
+
+  switch choosed_option {
+    case 0:
+      fmt.Println("exiting")
+    case 1:
+      fmt.Println("initializing the monitoring...")
+    case 2:
+      fmt.Println("showing logs")
+    default:
+      fmt.Println("not valid value choosed")
+  }
+}
+
+func welcome() {
   name := "andre"
   version := 1.1
 
   fmt.Println("Hello mr.", name)
   fmt.Println("this project is on version", version)
+}
 
+func showOptions() {
   fmt.Println("1 - initialize monitoring")
   fmt.Println("2 - show logs")
   fmt.Println("0 - exit")
+}
 
-  var choosed_option int
-  fmt.Scan(&choosed_option)
+func chooseOption() int {
+  var choosedOption int
+  fmt.Scan(&choosedOption)
+
+  return choosedOption
 }
