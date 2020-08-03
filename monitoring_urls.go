ef23c2c9137c901840d35f7e2ae9071986343159
Andre Oliveira <desmonplays@gmail.com>
2020-08-03 18:54:24 -0400

    feat: checking and returning the status of alura website

--- monitoring_urls.go
+++ monitoring_urls.go
@@ -5,47 +5,53 @@ import "os"
 import "net/http"
 
 func main() {
-  welcome()
-  showOptions()
-  choosedOption := chooseOption()
+	welcome()
+	showOptions()
+	choosedOption := chooseOption()
 
-  switch choosed_option {
-    case 0:
-      fmt.Println("exiting")
-    case 1:
-      monitoring()
-    case 2:
-      fmt.Println("showing logs")
-      os.Exit(0)
-    default:
-      fmt.Println("not valid value choosed")
-      os.Exit(-1)
-  }
+	switch choosedOption {
+	case 0:
+		fmt.Println("exiting")
+	case 1:
+		monitoring()
+	case 2:
+		fmt.Println("showing logs")
+		os.Exit(0)
+	default:
+		fmt.Println("not valid value choosed")
+		os.Exit(-1)
+	}
 }
 
 func welcome() {
-  name := "andre"
-  version := 1.1
+	name := "andre"
+	version := 1.1
 
-  fmt.Println("Hello mr.", name)
-  fmt.Println("this project is on version", version)
+	fmt.Println("Hello mr.", name)
+	fmt.Println("this project is on version", version)
 }
 
 func showOptions() {
-  fmt.Println("1 - initialize monitoring")
-  fmt.Println("2 - show logs")
-  fmt.Println("0 - exit")
+	fmt.Println("1 - initialize monitoring")
+	fmt.Println("2 - show logs")
+	fmt.Println("0 - exit")
 }
 
 func chooseOption() int {
-  var choosedOption int
-  fmt.Scan(&choosedOption)
+	var choosedOption int
+	fmt.Scan(&choosedOption)
 
-  return choosedOption
+	return choosedOption
 }
 
-func monitoring() { 
-  fmt.Println("initializing the monitoring...")
-  url := "www.alura.com.br"
-  resp, err := http.Get(url)
+func monitoring() {
+	fmt.Println("initializing the monitoring...")
+	url := "https://www.alura.com.br"
+	resp, _ := http.Get(url)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("The website:", url, "is up and running with success!")
+	} else {
+		fmt.Println("The website:", url, "is with problems")
+	}
 }
